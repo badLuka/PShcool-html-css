0fff42b6fff32fbe4974e9e192f7e01bfd896367
mk <mkalugin21@gmail.com>
2024-10-15 20:46:05 +0400

    updated comparison logic

--- 6-driver/driver.js
+++ 6-driver/driver.js
@@ -2,6 +2,6 @@ const hasLicence = true;
 const age = 19;
 const isDrunk = false;
 
-const canDrive = hasLicence && age > 18 && !isDrunk;
+const canDrive = hasLicence && age >= 18 && !isDrunk;
 
 console.log (`Могу вести машину:  ${canDrive ? 'Да' :  "Нет"} `);
